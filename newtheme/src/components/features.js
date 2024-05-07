3d890ecdd5e2309f71a0f2763fbe16b2da0bc955
faisal004 <faisalhusain1320@gmail.com>
2024-05-07 11:49:36 +0530

    changes

--- newtheme/src/components/features.js
+++ newtheme/src/components/features.js
@@ -1,33 +1,37 @@
 import React from "react";
-import about1 from '../assets/images/features/video-1.png'
-import about2 from '../assets/images/features/video-2.png'
-import about3 from '../assets/images/features/video-3.png'
+
+import feature2 from '../assets/images/features2.jpg'
+import feature3 from '../assets/images/features3.jpg'
+import feature4 from '../assets/images/features4.jpg'
+
 
 export default function Features({classlist}){
     const featuresData = [
         {
-            image:about1,
-            title:"$7000+ saved on video creation.",
-            desc:"Artificial intelligence makes it fast easy to create content for your video creations."
+            image:feature2,
+            title:"Agent Creation Functionality.",
+            desc:"Empower your workflow with CodeBolt's agent creation functionality. Agents tailored to your needs and preferences."
         },
         {
-            image:about2,
-            title:"35%+ increase in user engagement.",
-            desc:"Artificial intelligence makes it fast easy to create content for your video creations."
+            image:feature3,
+            title:"Seamless Model Integration.",
+            desc:" CodeBolt simplifies the integration of additional models into your agents, enhancing their capabilities with professionalism and ease."
         },
         {
-            image:about3,
-            title:"5 hours saved on single video.",
-            desc:"Artificial intelligence makes it fast easy to create content for your video creations."
+            image:feature4,
+            title:"Versatile Agent Selection.",
+            desc:" CodeBolt enables users to seamlessly switch between specialized agents for different tasks, optimizing efficiency and workflow management."
         },
     ]
     return(
         <>
             <div className={classlist}>
                 <div className="grid grid-cols-1 pb-6 text-center">
-                    <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">AI + video creation = <br/> outstanding <span className="bg-gradient-to-br from-amber-400 to-fuchsia-600 text-transparent bg-clip-text">Mortal.Ai</span></h3>
+                    <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">AI + CodeEditor = <br/> outstanding <span className="bg-gradient-to-br from-amber-400 to-fuchsia-600 text-transparent bg-clip-text">CodeBolt</span></h3>
 
-                    <p className="text-slate-400 max-w-xl mx-auto">Artificial intelligence makes it fast easy to create content for your blog, social media, website, and more!</p>
+                    <p className="text-slate-400 max-w-xl mx-auto">
+                    Imagine the intelligence of AI code generation combined with the versatility of agent-based workflow,welcome to Codebolt, where innovation meets efficiency!
+                    </p>
                 </div>
 
                 <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-6 gap-6">
@@ -35,7 +39,7 @@ export default function Features({classlist}){
                         return(
                         <div className="relative overflow-hidden bg-white dark:bg-slate-900 rounded-md shadow dark:shadow-gray-800" key={index}>
                             <div className="p-6 pb-0 relative overflow-hidden after:content-[''] after:absolute after:inset-0 after:mx-auto after:w-72 after:h-72 after:bg-gradient-to-tl after:to-amber-400 after:from-fuchsia-600 after:blur-[80px] after:rounded-full">
-                                <img src={item.image} className="relative rounded-t-md shadow-md dark:shadow-slate-700 z-1" alt=""/>
+                                <img src={item.image} className="relative rounded-t-md shadow-md dark:shadow-slate-700 z-1 h-72" alt=""/>
                             </div>
 
                             <div className="p-6">
