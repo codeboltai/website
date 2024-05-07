ad2b8279fc92fcdd021de00014a6399b5240ed3f
faisal004 <faisalhusain1320@gmail.com>
2024-05-07 14:32:43 +0530

    changes

--- newtheme/src/pages/contact.js
+++ newtheme/src/pages/contact.js
@@ -23,13 +23,7 @@ export default function Contact(){
             <meta name="description" content="This is utkarsh" />
         </Helmet>
         <Navbar/>
-        <div className="container-fluid relative mt-20">
-            <div className="grid grid-cols-1">
-                <div className="w-full leading-[0] border-0">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin" title="my-map" style={{border:"0"}} className="w-full h-[500px]" allowFullScreen></iframe>
-                </div>
-            </div>
-        </div>
+    
         <section className="relative lg:py-24 py-16">
             <div className="container">
                 <div className="grid md:grid-cols-12 grid-cols-1 items-center gap-[30px]">
@@ -114,24 +108,7 @@ export default function Contact(){
                         </div>
                     </div>
 
-                    <div className="text-center px-6">
-                        <div className="relative overflow-hidden text-transparent -m-3">
-                            <FiHexagon className="h-24 w-24 fill-amber-400/5 group-hover:fill-white/10 mx-auto"/>
-                            <div className="absolute top-2/4 -translate-y-2/4 start-0 end-0 mx-auto text-amber-400 rounded-xl group-hover:text-white duration-500 text-2xl flex align-middle justify-center items-center">
-                                <FiMapPin/>
-                            </div>
-                        </div>
-
-                        <div className="content mt-7">
-                            <h5 className="title h5 text-lg font-semibold">Location</h5>
-                            <p className="text-slate-400 mt-3">C/54 Northwest Freeway, Suite 558, <br/> Houston, USA 485</p>
-                            
-                            <div className="mt-5">
-                                <Link to="#"
-                                data-type="iframe" className="video-play-icon read-more lightbox hover:text-amber-400">View on Google map</Link>
-                            </div>
-                        </div>
-                    </div>
+               
                 </div>
             </div>
         </section>
