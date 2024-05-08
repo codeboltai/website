0dc482933ed2bbf1c6be46eefd439d88028cb3cc
faisal004 <faisalhusain1320@gmail.com>
2024-05-08 11:25:44 +0530

    changes

--- newtheme/src/pages/framework.js
+++ newtheme/src/pages/framework.js
@@ -23,18 +23,18 @@ export default function Framework(){
     const workData = [
         {
             icon:'mdi mdi-account-search-outline',
-            title:'Create Agent ',
-            desc:'By going through our CodeBolt documentation you will get to know how to create an agent.And tailor it to your needs.'
-        },
+            title:' Access Ready-to-Use AI Agents',
+            desc:'Discover a wide range of prebuilt AI agents in CodeSquad, powered by the Codebolt Framework. Avoid the complexities of building AI agents from scratch. Choose from our diverse library tailored to various tasks and industries, ensuring you find an agent that aligns with your business needs right from the start.'
+         },
         {
             icon:'mdi mdi-wallet-outline',
-            title:'Discribe Content',
-            desc:'If the distribution of letters and words  is random, the reader will not be distracted from making.'
+            title:'Customize Effortlessly with Codebolt Tools',
+            desc:'Leverage the Codebolt Frameworks advanced tools to easily customize your chosen AI agent. Adapt its behavior, responses, and overall functionality to meet your specific requirements. With intuitive controls and no need for deep coding skills, you can personalize the agent quickly and efficiently.'
         },
         {
             icon:'mdi mdi-home-plus-outline',
-            title:'Generate Content',
-            desc:'If the distribution of letters and words  is random, the reader will not be distracted from making.'
+            title:'Deploy Smoothly and Scale Effectively',
+            desc:'Implement your customized AI agent into your business systems without hassle, thanks to the robust integration features of the Codebolt Framework. This framework facilitates not only easy deployment but also ensures that your AI solutions are scalable, capable of handling varying interaction volumes to grow with your business needs.'
         },
     ]
     return(
@@ -45,18 +45,14 @@ export default function Framework(){
                 <div className="relative grid lg:grid-cols-12 grid-cols-1 items-center mt-10 gap-[30px]">
                     <div className="lg:col-span-7">
                         <div className="lg:me-6 lg:text-start text-center">
-                            <h1 className="font-bold lg:leading-normal leading-normal text-4xl lg:text-6xl mb-5">CodeSquad: <br/> Makes Developing Coding Agent Easy</h1>
-                            <h1> </h1>
+                            <h1 className="font-bold lg:leading-normal leading-normal text-4xl lg:text-6xl ">CodeSquad: </h1>
+                            <h1 className="font-bold lg:leading-normal leading-normal text-2xl lg:text-4xl mb-5"> Makes Developing Coding Agent Easy </h1>
                             <p className="text-lg max-w-xl lg:ms-0 mx-auto">Codebolt Ai Agent Framework makes it easy for anyone to write their custom AI Agents</p>
                         
-                            <div className="subcribe-form mt-6 mb-3">
-                                <form className="relative max-w-md mx-auto lg:ms-0">
-                                    {/* <div className="relative">
-                                        <i className="mdi mdi-email-outline text-xl absolute top-3 start-5"></i>
-                                        <input type="email" id="aiemail" name="email" className="py-4 pe-40 ps-12 w-full h-[50px] outline-none text-slate-900 dark:text-white rounded-md bg-white/60 dark:bg-slate-900/60 shadow dark:shadow-gray-800" placeholder="support@mortal.ai"/>
-                                    </div> */}
-                                    <button type="submit" className="py-2 px-5 inline-block font-semibold tracking-wide align-middle duration-500 text-base text-center absolute top-[2px] end-[3px] h-[46px] bg-amber-400 hover:bg-amber-500 border border-amber-400 hover:border-amber-500 text-white rounded-md">Get Started</button>
-                                </form>
+                            <div className="subcribe-form mt-6 mb-3  w-full">
+                            <button type="submit" className="py-2 px-5 inline-block font-semibold tracking-wide align-middle duration-500 text-base text-center  h-[46px] bg-amber-400 hover:bg-amber-500 border border-amber-400 hover:border-amber-500 text-white rounded-md">Get Started</button>
+
+                              
                             </div>
                         </div>
                     </div>
@@ -86,7 +82,7 @@ export default function Framework(){
                 <div className="grid md:grid-cols-3 grid-cols-1 mt-6 gap-6">
                     {workData.map((item,index)=>{
                         return(
-                            <div className="relative p-6" key={index}>
+                            <div className="relative p-6 border-2 rounded-xl" key={index}>
                                 <i className={`${item.icon} bg-gradient-to-tl to-amber-400 from-fuchsia-600 bg-clip-text text-[45px]`}></i>        
 
                                 <h5 className="text-xl font-semibold my-5">{item.title}</h5>
