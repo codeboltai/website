030d526752137d5298a381d99394bcd451d32980
faisal004 <faisalhusain1320@gmail.com>
2024-07-08 14:25:20 +0530

    changes

--- components/index/aboutTwo.tsx
+++ components/index/aboutTwo.tsx
@@ -61,13 +61,14 @@ export default function AgentAboutTwo() {
               </Link>
             </div>
           </div>
-          <div className="relative overflow-hidden rounded-lg border border-amber-400/5 bg-gradient-to-tl to-amber-400/30  from-fuchsia-600/30 dark:to-amber-400/50 dark:from-fuchsia-600/50">
+          <div className="relative overflow-hidden rounded-lg  border-amber-400/5 bg-gradient-to-tl to-amber-400/30  from-fuchsia-600/30 dark:to-amber-400/50 dark:from-fuchsia-600/50">
             <Image
             height={800}
             width={800}
             src={feature4}
             className="ltr:rounded-tl-lg rtl:rounded-tr-lg"
             alt=""
+            
             />
           </div>
         </div>
