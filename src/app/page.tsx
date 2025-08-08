4d4a1b82d01990f7aa26de841ac612075bd2b215
Utkarsh Shukla <utk.shukla@gmail.com>
2025-08-08 09:39:00 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/sections/Hero'
 import Features from '@/components/sections/Features'
+import MajorFeatures from '@/components/sections/MajorFeatures'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <MajorFeatures />
     </>
   )
 }
